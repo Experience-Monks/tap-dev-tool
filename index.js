cd2301a38f46d8c4f8cedb122004f8c15496c944
mattdesl <dave.des@gmail.com>
2015-06-12 10:41:54 -0400

    errors are logged to console since this is a development-specific tool

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ module.exports = function errorify (b) {
     var output = through(write)
     var pipeline = bundle(cb)
     pipeline.pipe(output)
+    pipeline.once('error', function (err) {
+      console.error('error: %s', err)
+      var result = errMessage(err)
+      output.push('!console.error(' + JSON.stringify(result) + ');')
+      output.push(null)
+      pipeline.unpipe(output)
+      pipeline.on('error', function (err2) {
+        // module-deps likes to emit each error
+        console.error('error: %s', err2)
+      })
+    })
     return output
   }
 
@@ -24,5 +35,13 @@ module.exports = function errorify (b) {
     }
     this.push(buf)
     next()
+  }
+
+  function errMessage (err) {
+    if (err.codeFrame) { //babelify@6.x
+      return [err.message, err.codeFrame].join('\n\n')
+    } else { //babelify@5.x and browserify
+      return err.annotated || err.message
+    }
   }
 }
