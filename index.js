02bdc3bf5c1f58dcfcaa2ae4f5b160834a39589d
mattdesl <dave.des@gmail.com>
2015-06-16 11:33:57 -0400

    remove error logging and just emit error to be less surprising

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path = require('path')
 var build = path.join(__dirname, 'dist', 'build.js')
 var prelude = fs.readFileSync(build)
 
-module.exports = function errorify (b) {
+module.exports = function tapDevTool (b) {
   var written = false
   var bundle = b.bundle.bind(b)
   b.bundle = function (cb) {
@@ -14,16 +14,8 @@ module.exports = function errorify (b) {
     var output = through(write)
     var pipeline = bundle(cb)
     pipeline.pipe(output)
-    pipeline.once('error', function (err) {
-      console.error('error: %s', err)
-      var result = errMessage(err)
-      output.push('!console.error(' + JSON.stringify(result) + ');')
-      output.push(null)
-      pipeline.unpipe(output)
-      pipeline.on('error', function (err2) {
-        // module-deps likes to emit each error
-        console.error('error: %s', err2)
-      })
+    pipeline.on('error', function (err) {
+      output.emit('error', err)
     })
     return output
   }
@@ -35,13 +27,5 @@ module.exports = function errorify (b) {
     }
     this.push(buf)
     next()
-  }
-
-  function errMessage (err) {
-    if (err.codeFrame) { // babelify@6.x
-      return [err.message, err.codeFrame].join('\n\n')
-    } else { // babelify@5.x and browserify
-      return err.annotated || err.message
-    }
   }
 }
